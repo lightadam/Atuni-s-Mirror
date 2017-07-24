179921eadbfaf5b04671bc16e2491d379cbc6478
satsumao <matthewduckworth100@gmail.com>
2017-07-23 22:44:27 -0500

    Add files via upload

---
+++ Collision.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+
+    public Control movement;
+    public bool onSomething;
+    public Transform grounder;
+    public float radius;
+    public LayerMask ground;
+
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        onSomething = Physics2D.OverlapCircle(grounder.transform.position, radius, ground);
+
+        if (collisionInfo.gameObject.tag == "Platform" && onSomething == false)
+        {
+            movement.enabled = false;
+        }
+        else
+        {
+            movement.enabled = true;
+        }
+    }
+
+}
---
+++ FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+    public Transform player;
+    public Vector3 rightCamera;
+    public Vector3 leftCamera;
+    public Vector3 offset;
+   
+	void Update () {
+        transform.position = player.position + offset;
+        if (Input.GetKey("d"))
+        {
+            transform.position = player.position + rightCamera;
+        }
+         else if (Input.GetKey("a")) {
+            transform.position = player.position  + leftCamera;
+        }
+    }
+}
